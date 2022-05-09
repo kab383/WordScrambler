f764ed9293c50a421ddd82d5dc0c1ee9aa301dfc
Kristian Beall <beallkristian@gmail.com>
2022-05-09 14:15:51 -0400

    added code for the seed data method and completed the fake database in the Words file

--- WordScrambler.ConsoleApp/Program.cs
+++ WordScrambler.ConsoleApp/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WordScrambler.ConsoleApp;
+UserInterface ui = new UserInterface();
+
+ui.Run();
--- WordScrambler.ConsoleApp/UserInterface.cs
+++ WordScrambler.ConsoleApp/UserInterface.cs
@@ -2,11 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WordScrambler.Repository;
 
 namespace WordScrambler.ConsoleApp
 {
     public class UserInterface
     {
-        
+    
+        private bool isRunning = true;
+
+        WordScramblerRepository wsrRepo = new WordScramblerRepository();
+
+        public void Run()
+        {
+            wsrRepo.SeedWordData();
+
+            while(isRunning)
+            {
+                PrintMainMenu();
+
+                string input = GetUserInput();
+            }
+        }
+
+        private void PrintMainMenu()
+        {
+            Console.WriteLine("Welcome to Word Scrambler!\n" +
+            "You have 3 attempts to guess each word. There will be 5 rounds.\n" +
+            "Enter a difficulty to start the game:\n" +
+            "1. I know my ABC's...\n" +
+            "2. Average vocabulary\n" +
+            "3. Wordsmith\n" +
+            "4. Spectacular vernacular\n" +
+            "5. My name is Merriam-Webster");
+        }
+
+        private string GetUserInput()
+        {
+            return Console.ReadLine();
+        }
+
     }
 }
