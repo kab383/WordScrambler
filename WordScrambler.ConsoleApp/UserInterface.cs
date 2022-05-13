a4fa21489dd36a6c7cd34391f452eca3144eee8b
Kristian Beall <beallkristian@gmail.com>
2022-05-12 21:31:05 -0400

    operational word scrambler game - v1

--- WordScrambler.ConsoleApp/UserInterface.cs
+++ WordScrambler.ConsoleApp/UserInterface.cs
@@ -10,17 +10,26 @@ namespace WordScrambler.ConsoleApp
     {
     
         private bool isRunning = true;
-
         WordScramblerRepository wsrRepo = new WordScramblerRepository();
+
 
         public void Run()
         {
             wsrRepo.SeedWordData();
+            // List<Words> scrambledWords = wsrRepo.ShuffleWordsGroup(wsrRepo._wordsListDB);
 
+            // Console.WriteLine(scrambledWords[1].ZooAnimals); //should print ilno
+
+            // Console.WriteLine(scrambledWords);
+            // ^ convert to variable later to reference
+            
+            // Console.WriteLine(wsrRepo.ShuffleString("hello"));
+            
+            // // wsrRepo.ShuffleString(wsrRepo._wordsListDB);
             while(isRunning)
             {
                 PrintMainMenu();
-
+                
                 string input = GetUserInput();
 
                 UserInputSwitchCase(input);
@@ -30,7 +39,7 @@ namespace WordScrambler.ConsoleApp
         private void PrintMainMenu()
         {
             Console.Write("Welcome to Word Scrambler!\n" +
-            "You have 3 attempts to guess each word. There will be 5 rounds.\n" +
+            "You have 1 attempt to guess each word. There will be 10 rounds.\n" +
             "1. I know my ABC's\n" +
             "2. Average vocabulary\n" +
             "3. Wordsmith\n" +
@@ -48,25 +57,61 @@ namespace WordScrambler.ConsoleApp
             switch(input)
             {
                 case "1":
-                //not sure yet. start game 1, and so on?
+                    StartGame("iKnowMyABC");
                     break;
+                case "2":
+                    StartGame("averageVocabulary");
+                    break;
+                case "3":
+                    StartGame("wordSmith");
+                    break;
+                case "4":
+                    StartGame("spectacularVernacular");
+                    break;
+                case "5":
+                    StartGame("myNameIsMerriamWebster");
+                    break;
+                default:
+                    Console.WriteLine("Try another entry.");
+                    break;       
             }
         }
 
-        // private string GetUserAnswer()
-        // {
-        //     return Console.ReadLine();
-        // }
-        public string StarrtRound(string answer)
+        public void StartGame(string groupTitle)
         {
-            // NEEDS WORK
-            Console.WriteLine("Unscramble this word ${}");
-            GetUserAnswer();
+            Words unscrambledWords = wsrRepo.GetWordsByGroupTitle(groupTitle);
+
+            string[] unscrambledArr = {unscrambledWords.ZooAnimals, unscrambledWords.Presidents, unscrambledWords.Countries, unscrambledWords.Movies, unscrambledWords.Foods, unscrambledWords.Sports, unscrambledWords.TvShows, unscrambledWords.Brands, unscrambledWords.Desserts};
+
+            Words scrambledWords = wsrRepo.ShuffleWordsGroup(unscrambledWords);
+
+            string[] scrambledArr = {scrambledWords.ZooAnimals, scrambledWords.Presidents, scrambledWords.Countries, scrambledWords.Movies, scrambledWords.Foods, scrambledWords.Sports, scrambledWords.TvShows, scrambledWords.Brands, scrambledWords.Desserts};
+
+            // While loop to continue game, or end it with a boolean
+
+                for(int i = 0; i < 9; i++ )
+                {
+                    Console.Write("Scrambled Word: ");
+                    Console.WriteLine(scrambledArr[i]);
+
+                    string guess = GetUserInput();
+
+                    if(guess.ToUpper() == unscrambledArr[i].ToUpper())
+                    {
+                        Console.WriteLine("Correct! Next Question...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect! Sorry, back to main menu... try again.");
+                        break;
+                    }
+            }
         }
-
-        // PrintScore()
-
-        // PlayAgain()
-
     }
 }
+        
+
+        // PrintScore() - stretch goal.
+
+        // PlayAgain()
+        // Console.WriteLine conditional to play again and PrintMainMenu() or quit game with a see you later message.
